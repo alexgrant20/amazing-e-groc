0a541995c12c5268f3e7283a7c7b5818ca9f8f11
Steven Alexander <alexgrant200@yahoo.com>
2023-02-08 14:49:35 +0700

    Adding basic template structure

---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layout.head')
+
+<body>
+   @include('layout.header')
+   @yield('container')
+
+   @include('layout.footer')
+
+   @include('layout.foot')
+</body>
+
+</html>
